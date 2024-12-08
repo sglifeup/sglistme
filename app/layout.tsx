8d7e816a43686f46a159a1c76dd7740f7dc9c9a2
Ren Xiang <ideasxiang@gmail.com>
2024-12-08 19:52:10 +0800

    speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
