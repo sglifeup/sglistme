83d469b5fb264605eb0fb432a6aabd85c1ea8e81
Ren Xiang <ideasxiang@gmail.com>
2024-12-08 19:44:40 +0800

    favicon 5

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   title: 'SG Property Listings',
   description: 'Live Property Listings in Singapore',
   icons: {
-    icon: '/LIST(1).png',
+    icon: '/LIST(4).png',
   },
 }
 
